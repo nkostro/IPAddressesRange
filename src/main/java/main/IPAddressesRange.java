b259aabc34446563400b5083c54fe98c05211e63
Nikolai Kostromitin <emailbin@yandex.ru>
2018-12-11 09:38:12 +0500

    Add dummy program with dummy test.

---
+++ src/main/java/main/IPAddressesRange.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Range of IP addresses.
+ */
+public class IPAddressesRange {
+    public static void main( String[] args ) {
+    }
+}
+
---
+++ src/test/java/main/IPAddressesRangeTest.java
@@ -0,0 +1,15 @@
+package main;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+/**
+ * Unit test for IP addresses range program.
+ */
+public class IPAddressesRangeTest
+{
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
